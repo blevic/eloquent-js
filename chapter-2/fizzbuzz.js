5e486dfcdb28636d05e0ca6ef4f8049908af1dff
Breno Levi <blevic@proton.me>
2022-07-27 00:26:26 -0300

    Slightly better version of fizzbuzz

--- chapter-2/fizzbuzz.js
+++ chapter-2/fizzbuzz.js
@@ -1,11 +1,8 @@
 for (let number = 1; number <= 100; number += 1) {
-    if (number%3 == 0 && number%5 == 0) {
-        console.log("FizzBuzz")
-    } else if (number%3 == 0) {
-        console.log("Fizz")
-    } else if (number%5 == 0) {
-        console.log("Buzz")
-    } else {
-    console.log(number)
-    }
+    if (number%3 == 0 && number%5 == 0) out = "FizzBuzz";
+    else if (number%3 == 0) out = "Fizz";
+    else if (number%5 == 0) out = "Buzz";
+    else out = number.toString();
+
+    console.log(out)
 }
