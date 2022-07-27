3318a79db734c8fb95956f84e31ead6eeaffe909
Breno Levi <blevic@proton.me>
2022-07-27 00:10:54 -0300

    Better code

--- chapter-4/the-sum-of-a-range.js
+++ chapter-4/the-sum-of-a-range.js
@@ -10,27 +10,25 @@ console.log(range(1, 10));
 
 function sum(array) {
   var sum = 0;
-  for (var i = 0; i < array.length; i++) {
-    sum += array[i];
+  for (var element of array) {
+    sum += element;
   }
   return sum;
 }
 
 console.log(sum(range(1, 10)));
 
-function range_modified(start, end, step) {
+function range_modified(start, end, step = start < end ? 1 : -1) {
   var sequence = [];
   if (step > 0) {
-    for (var i = start; i <= end; i += step) {
-      sequence.push(i);
-    }
+    for (var i = start; i <= end; i += step) sequence.push(i);
   } else {
-    for (var i = start; i >= end; i += step) {
-        sequence.push(i);
-    }
-    }
-    return sequence;
+    for (var i = start; i >= end; i += step) sequence.push(i);
+  }
+  
+  return sequence;
 }
 
+console.log(range_modified(1, 10));
 console.log(range_modified(1, 10, 2));
 console.log(range_modified(5, 2, -1));
