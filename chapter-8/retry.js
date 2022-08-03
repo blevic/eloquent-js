6dab6787eeeac9ef25256e47959091e4118b6b29
Breno Levi <blevic@proton.me>
2022-08-02 23:57:47 -0300

    Create wrapper function

--- chapter-8/retry.js
+++ chapter-8/retry.js
@@ -7,3 +7,21 @@ function primitiveMultiply(a, b) {
 
   throw new MultiplicatorUnitFailure("My bad, I failed!");
 }
+
+function wrapper() {
+  for (;;) {
+    try {
+        myNumber = primitiveMultiply(2, 3);
+        console.log(`The multiplication 2 * 3 is: ${myNumber}`);
+        break;
+    } catch (e) {
+        if (e instanceof MultiplicatorUnitFailure) {
+        console.log("Caught MultiplicatorUnitFailure. Let's try again!");
+        } else {
+        throw e;
+        }
+    }
+  }
+}
+
+wrapper();
